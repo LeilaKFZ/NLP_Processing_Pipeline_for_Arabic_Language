d150ccbbcc85afd3c4389fb44784d7e890613c96
Fatima.Z  Leila Keddar <116590538+LeilaKFZ@users.noreply.github.com>
2025-01-01 17:10:28 +0100

    Update dictionary.py

--- Modules/dictionary.py
+++ Modules/dictionary.py
@@ -1,7 +1,7 @@
 import os
 import re
 
-# Ouvrir les fichiers vocabulaire et non-vocabulaire
+# open both files
 with open("../Datas/dictionary.txt", "r", encoding='utf-8') as f_dict:
     dict_start = set(f_dict.read().splitlines())
 with open("../Datas/vocabulary.txt", "w", encoding='utf-8') as f_vocab:
@@ -9,14 +9,12 @@ with open("../Datas/vocabulary.txt", "w", encoding='utf-8') as f_vocab:
 with open("../Datas/non_dict.txt", "w", encoding='utf-8') as f_not:
     pass
 
-# Définir le chemin d'accès au dossier
 dossier = "../train_tk"
 
-# Fonction d'analyse de fichiers
+###### fct
 def analyse_fichier(fichier):
     with open(os.path.join(dossier, fichier), "r", encoding='utf-8') as f:
         texte = f.read()
-    # Tokeniser le texte (déjà tokenisé dans votre cas)
     # mots = re.findall(r"\w+", texte)
     mots = texte.split()
     for mot in mots:
@@ -29,7 +27,7 @@ def analyse_fichier(fichier):
             with open("../Datas/non_dict.txt", "a", encoding='utf-8') as f_not:
                 f_not.write(mot + "\n")
 
-# Analyser tous les fichiers du dossier
+##### Begine the analyse
 for fichier in os.listdir(dossier):
     if fichier.endswith(".txt"):
         analyse_fichier(fichier)
